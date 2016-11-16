6dee6c2b66de48540ee1e78a10d4d28871b35e6c
Phornphawit Manasut <wittz0014@gmail.com>
2016-11-16 09:06:17 +0700

    fixing bug

--- src/main/java/io/muic/dcom/p2/DataModel.java
+++ src/main/java/io/muic/dcom/p2/DataModel.java
@@ -38,21 +38,15 @@ public class DataModel {
         public String getStationId() { return stationId; }
         public long getTimeStamp() { return timeStamp; }
     }
-    private ConcurrentHashMap<String,ConcurrentSkipListSet<ParcelObserved>>[] parcelTrailWriter;
-    private ConcurrentHashMap<String,Long>[] stationCountWriter;
-//    private ConcurrentHashMap<String,ConcurrentSkipListSet<ParcelObserved>>[] parcelTrailGetter;
-//    private ConcurrentHashMap<String,Long>[] stationCountGetter;
+    ConcurrentHashMap<String,ConcurrentSkipListSet<ParcelObserved>>[] parcelTrailWriter;
+    ConcurrentHashMap<String,Long>[] stationCountWriter;
 
     DataModel() {
         parcelTrailWriter = new ConcurrentHashMap[DataConfig.SIZE];
         stationCountWriter = new ConcurrentHashMap[DataConfig.SIZE];
-//        parcelTrailGetter = new ConcurrentHashMap[DataConfig.SIZE];
-//        stationCountGetter = new ConcurrentHashMap[DataConfig.SIZE];
         for(int i = 0; i < DataConfig.SIZE; i++){
             parcelTrailWriter[i] = new ConcurrentHashMap<>();
             stationCountWriter[i] = new ConcurrentHashMap<>();
-//            parcelTrailGetter[i] = new ConcurrentHashMap<>();
-//            stationCountGetter[i] = new ConcurrentHashMap<>();
         }
     }
 
@@ -61,25 +55,7 @@ public class DataModel {
         int s = extractSlot(parcelId);
         addParcelTrail(s,parcelId,newp);
         incrementStationStopCount(s,stationId);
-//        ConcurrentSkipListSet<ParcelObserved> t = getPath(s,parcelId);
-//        Integer stopCount = getStationStopCount(s,stationId);
-//        replace(s,parcelId,t);
-//        replace(s,stationId,stopCount);
     }
-
-//    public ConcurrentSkipListSet<ParcelObserved> getPath(Integer slot, String id){
-//        ConcurrentSkipListSet<ParcelObserved> trail = parcelTrailWriter[slot].get(id).clone();
-//        return trail;
-//    }
-//    public Long getStationStopCount(Integer slot, String id){
-//        return stationCountWriter[slot].get(id);
-//    }
-//    public void replace(Integer slot, String id, Integer stopCount){
-//        stationCountGetter[slot].put(id,stopCount);
-//    }
-//    public void replace(Integer slot,String id, ConcurrentSkipListSet<ParcelObserved> trail){
-//        parcelTrailGetter[slot].put(id,trail);
-//    }
 
     public int extractSlot(String id){
         return DataConfig.PARCEL_PREFIX.get(String.valueOf(id.charAt(0)).toLowerCase());
@@ -90,7 +66,6 @@ public class DataModel {
             parcelTrailWriter[slot].put(parcelId, new ConcurrentSkipListSet<>());
         }
         parcelTrailWriter[slot].get(parcelId).add(parcelObserved);
-
     }
 
     public void incrementStationStopCount(int slot, String stationId){
@@ -111,6 +86,7 @@ public class DataModel {
     }
     public long getStopCount(String stationId) {
         int slot = extractSlot(stationId);
+        System.out.println(slot + " " + stationCountWriter[slot].containsKey(stationId));
         if(!stationCountWriter[slot].containsKey(stationId)){return 0;}
         else{return stationCountWriter[slot].get(stationId);}
     }
